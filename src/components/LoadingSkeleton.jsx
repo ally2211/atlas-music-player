be10038ccbee2cc0b35592f47b2a8cad7926f82c
ally2211 <amendieta.va@gmail.com>
2026-01-21 16:40:37 -0600

    loadingSkeleton dark mode support

--- src/components/LoadingSkeleton.jsx
+++ src/components/LoadingSkeleton.jsx
@@ -2,18 +2,48 @@ import React from 'react';
 
 export default function LoadingSkeleton() {
     return (
-        <div className="animate-pulse space-y-4 p-4 max-w-md mx-auto">
-            {/* Simulated title */}
-            <div className="h-6 bg-gray-300 dark:bg-gray-700 rounded w-3/4"></div>
+        <div className="flex flex-col md:flex-row gap-8 max-w-[900px] mx-auto p-4 sm:p-8 animate-pulse">
+            {/* Player Column Skeleton */}
+            <div className="w-full md:w-1/2 space-y-6">
+                {/* Cover Art Skeleton */}
+                <div className="w-full aspect-square bg-gray-300 dark:bg-gray-700 rounded"></div>
 
-            {/* Simulated media/image block */}
-            <div className="h-48 bg-gray-300 dark:bg-gray-700 rounded"></div>
+                {/* Song Title Skeleton */}
+                <div className="space-y-2">
+                    <div className="h-8 bg-gray-300 dark:bg-gray-700 rounded w-3/4"></div>
+                    <div className="h-4 bg-gray-300 dark:bg-gray-700 rounded w-1/2"></div>
+                </div>
 
-            {/* Simulated text lines */}
-            <div className="space-y-2">
-                <div className="h-4 bg-gray-300 dark:bg-gray-700 rounded w-full"></div>
-                <div className="h-4 bg-gray-300 dark:bg-gray-700 rounded w-5/6"></div>
-                <div className="h-4 bg-gray-300 dark:bg-gray-700 rounded w-2/3"></div>
+                {/* Play Controls Skeleton */}
+                <div className="flex justify-between gap-4">
+                    <div className="h-10 w-10 bg-gray-300 dark:bg-gray-700 rounded"></div>
+                    <div className="h-10 w-10 bg-gray-300 dark:bg-gray-700 rounded"></div>
+                    <div className="h-12 w-12 bg-gray-300 dark:bg-gray-700 rounded"></div>
+                    <div className="h-10 w-10 bg-gray-300 dark:bg-gray-700 rounded"></div>
+                    <div className="h-10 w-10 bg-gray-300 dark:bg-gray-700 rounded"></div>
+                </div>
+
+                {/* Volume Control Skeleton */}
+                <div className="h-2 bg-gray-300 dark:bg-gray-700 rounded w-full"></div>
+            </div>
+
+            {/* Playlist Column Skeleton */}
+            <div className="w-full md:w-1/2 space-y-4">
+                <div className="h-6 bg-gray-300 dark:bg-gray-700 rounded w-24"></div>
+                <div className="space-y-3">
+                    <div className="border border-gray-300 dark:border-gray-600 rounded-lg p-3">
+                        <div className="h-5 bg-gray-300 dark:bg-gray-700 rounded w-3/4 mb-2"></div>
+                        <div className="h-4 bg-gray-300 dark:bg-gray-700 rounded w-1/2"></div>
+                    </div>
+                    <div className="border border-gray-300 dark:border-gray-600 rounded-lg p-3">
+                        <div className="h-5 bg-gray-300 dark:bg-gray-700 rounded w-3/4 mb-2"></div>
+                        <div className="h-4 bg-gray-300 dark:bg-gray-700 rounded w-1/2"></div>
+                    </div>
+                    <div className="border border-gray-300 dark:border-gray-600 rounded-lg p-3">
+                        <div className="h-5 bg-gray-300 dark:bg-gray-700 rounded w-3/4 mb-2"></div>
+                        <div className="h-4 bg-gray-300 dark:bg-gray-700 rounded w-1/2"></div>
+                    </div>
+                </div>
             </div>
         </div>
     );
